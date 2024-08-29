0a9db7d1347b617cd4166338444e7d26ae458ad1
Vo Dang Trinh <109226619+hyperzone14@users.noreply.github.com>
2024-08-29 20:47:35 +0700

    adding the Mapping for the symptom without the uses

--- src/main/java/com/example/clinic_management/mapper/AutoSymptomMapper.java
+++ src/main/java/com/example/clinic_management/mapper/AutoSymptomMapper.java
@@ -5,6 +5,7 @@ import com.example.clinic_management.dtos.responses.SymptomResponseDTO;
 import com.example.clinic_management.entities.Symptom;
 import org.mapstruct.Mapper;
 
+@Mapper(componentModel = "spring")
 public interface AutoSymptomMapper {
     Symptom toEntity(SymptomRequestDTO symptomRequestDTO);
     SymptomResponseDTO toResponseDTO(Symptom symptom);
