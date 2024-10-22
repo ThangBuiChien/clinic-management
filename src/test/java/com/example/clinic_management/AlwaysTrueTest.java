5ad77025bc1da241ebbc27a69ac7959b5c5a6e43
BuiChienThang <107343290+ThangBuiChien@users.noreply.github.com>
2024-10-22 13:12:18 +0700

    Create AlwaysTrueTest.java

---
+++ src/test/java/com/example/clinic_management/AlwaysTrueTest.java
@@ -0,0 +1,12 @@
+package com.example.clinic_management;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AlwaysTrueTest {
+    @Test
+    void testAlwaysTrue() {
+        assertTrue(false);  // This will always pass
+    }
+}
