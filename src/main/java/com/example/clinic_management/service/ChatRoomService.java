a9eb6921e68d88f2460c27db1ca25e0fb318642c
BuiChienThang <107343290+ThangBuiChien@users.noreply.github.com>
2024-10-28 16:30:18 +0700

    Add chatRoomResponseDTO

--- src/main/java/com/example/clinic_management/service/ChatRoomService.java
+++ src/main/java/com/example/clinic_management/service/ChatRoomService.java
@@ -3,11 +3,12 @@ package com.example.clinic_management.service;
 import java.util.List;
 
 import com.example.clinic_management.dtos.responses.ChatMessageResponseDTO;
+import com.example.clinic_management.dtos.responses.ChatRoomResponseDTO;
 import com.example.clinic_management.entities.ChatMessage;
 import com.example.clinic_management.entities.ChatRoom;
 
 public interface ChatRoomService {
-    public ChatRoom createChatRoom(String roomName, List<Long> userIds);
+    public ChatRoomResponseDTO createChatRoom(String roomName, List<Long> userIds);
 
     public List<ChatMessageResponseDTO> getChatMessageHistory(Long chatRoomId);
 
