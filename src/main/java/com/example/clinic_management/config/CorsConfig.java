5ca7a3e3e631a317d03133d975e6c1b8ae62770f
BuiChienThang <107343290+ThangBuiChien@users.noreply.github.com>
2025-05-02 22:10:49 +0700

    Update CorsConfig.java

--- src/main/java/com/example/clinic_management/config/CorsConfig.java
+++ src/main/java/com/example/clinic_management/config/CorsConfig.java
@@ -1,15 +1,21 @@
 package com.example.clinic_management.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+
+    @Value("${FE.url}")
+    private String frontendUrl;
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        frontendUrl)
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
