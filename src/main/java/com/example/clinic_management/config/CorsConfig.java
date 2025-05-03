30f1ceeaccae56738eb9d7150ef7f35c49ec6b47
BuiChienThang <107343290+ThangBuiChien@users.noreply.github.com>
2025-05-03 16:40:47 +0700

    Revert "Update CorsConfig.java"
    This reverts commit c9e07692e9e223d590282c15fe86fa58c05cf5fb.

--- src/main/java/com/example/clinic_management/config/CorsConfig.java
+++ src/main/java/com/example/clinic_management/config/CorsConfig.java
@@ -8,10 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-//    @Value("${FE.url}")
-//    private String frontendUrl;
-
-    @Value("${FE.url:https://clinic-management-fe-flame.vercel.app}")
+    @Value("${FE.url}")
     private String frontendUrl;
     @Override
     public void addCorsMappings(CorsRegistry registry) {
