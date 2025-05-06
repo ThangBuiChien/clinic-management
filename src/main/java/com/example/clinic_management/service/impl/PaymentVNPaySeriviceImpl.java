7acc3208fc733709f56891b671f9bed32a435e4b
BuiChienThang <107343290+ThangBuiChien@users.noreply.github.com>
2025-05-06 22:16:29 +0700

    Update PaymentVNPaySeriviceImpl.java

--- src/main/java/com/example/clinic_management/service/impl/PaymentVNPaySeriviceImpl.java
+++ src/main/java/com/example/clinic_management/service/impl/PaymentVNPaySeriviceImpl.java
@@ -76,8 +76,8 @@ public class PaymentVNPaySeriviceImpl implements PaymentVNPayService {
         vnp_Params.put("vnp_ReturnUrl", PaymentConfig.vnp_ReturnUrl);
         vnp_Params.put("vnp_IpAddr", PaymentConfig.getIpAddress(req));
 
-//        Calendar cld = Calendar.getInstance(TimeZone.getTimeZone("Etc/GMT+7"));
-        Calendar cld = Calendar.getInstance(TimeZone.getDefault());
+        Calendar cld = Calendar.getInstance(TimeZone.getTimeZone("Etc/GMT+7"));
+//        Calendar cld = Calendar.getInstance(TimeZone.getDefault());
         SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
         String vnp_CreateDate = formatter.format(cld.getTime());
         vnp_Params.put("vnp_CreateDate", vnp_CreateDate);
